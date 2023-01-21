67529f5ee9f44e2a141ebd2524223932ea7e8e39
antonius <antoniusardyyansah@gmail.com>
2023-01-21 22:08:15 +0700

    todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+// interface
+interface TodoList {
+  text: string,
+  checked: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoList';
+  title = 'todo-app';
+  list: TodoList[]
+  txtItem = ''
+
+  constructor() {
+    this.list = []
+  }
+
+  addItem() {
+    this.list.push({ text: this.txtItem, checked: false})
+    this.txtItem = ''
+  }
+
+  completeItem(item: any) {
+    if(item.checked == false) {
+      item.checked = true
+    } else {
+      item.checked = false
+    }
+  }
+
+  deleteItem(index: number) {
+    this.list.splice(index, 1)
+  }
+
+  deleteAll () {
+    let todoLength = this.list.length
+    this.list.splice(0, todoLength)
+  }
 }
